99d48d0759eeac67540bcb035c198040698af6d7
Gabriel Biren <gbiren@google.com>
2022-10-28 18:54:04 +0000

    gtaxl-common: Use the AIDL WiFi service
    Bug: 205044134
    Test: Presubmit tests
    Change-Id: Icdaa448e2ed59b638056593adb4bcd769f34220d

--- common.mk
+++ common.mk
@@ -264,7 +264,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     libwpa_client \
     wificond \
