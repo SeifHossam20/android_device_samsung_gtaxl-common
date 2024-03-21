8c94875f877973b2dd842175ff9c8ead0ea151c5
Bruno Martins <bgcngm@gmail.com>
2024-03-21 16:31:24 +0000

    gtaxl-common: Remove deprecated trust HAL
    Change-Id: Ic5ac79b4ca60b87a97622877b97f806c07cab907

--- common.mk
+++ common.mk
@@ -244,10 +244,6 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.samsung
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
