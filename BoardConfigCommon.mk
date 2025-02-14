e139320095444ba514a3d55915d75bdd951339fe
Seif Hossam <seifhossam862@gmail.com>
2025-02-14 07:01:27 +0200

    Update BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -67,7 +67,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/framework_compatibility_matrix.xml \
-vendor/lineage/config/device_framework_matrix.xml
+vendor/superior/config/device_framework_matrix.xml
 
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
